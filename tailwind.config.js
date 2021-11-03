130dd7e12c441747b4a44f71e60522f327b484e6
sham <shamlo_redzx@yahoo.com>
2021-11-03 20:56:11 +0300

    Tailwind Configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 module.exports = {
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    mode: 'jit',
+
+    purge: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+        './public/**/*.html',
+        './src/**/*.{js,jsx,ts,tsx,vue}',
+    ],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
+        screens: {
+            sm: '640px',
+            // => @media (min-width: 640px) { ... }
+
+            md: '768px',
+            // => @media (min-width: 768px) { ... }
+
+            lg: '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            xl: '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+            lgm: '1025px',
+        },
     },
     variants: {
         extend: {},
