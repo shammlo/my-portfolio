70269b478811e4dafc69919d85021ddb033f06f1
sham <shamlo_redzx@yahoo.com>
2021-11-03 21:07:01 +0300

    _app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 // import '../styles/globals.css'
 import '../styles/sass/main.scss';
+import Layout from '../Layout/Layout';
 import type { AppProps } from 'next/app';
-
+import 'aos/dist/aos.css';
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
 export default MyApp;
