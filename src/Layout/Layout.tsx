34b15365f936576c1d2d0f8184e48786facc0932
sham <shamlo_redzx@yahoo.com>
2021-11-04 19:57:07 +0300

    Adjusting animations for better user experience

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -27,7 +27,7 @@ const Layout: React.FC<LayoutProps> = (props) => {
             delay: 250, // values from 0 to 3000, with step 50ms
             duration: 1200, // values from 0 to 3000, with step 50ms
             easing: 'ease', // default easing for AOS animations
-            once: false, // whether animation should happen only once - while scrolling down
+            once: true, // whether animation should happen only once - while scrolling down
             mirror: true, // whether elements should animate out while scrolling past them
             anchorPlacement: 'top-bottom',
             useClassNames: false,
