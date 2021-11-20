b60c717bd871f7f10e8ac4d364b353ca92b8552c
sham <shamlo_redzx@yahoo.com>
2021-11-20 23:07:51 +0300

    Removed unnecessary tailwind classes

--- src/Layout/Banner.tsx
+++ src/Layout/Banner.tsx
@@ -81,11 +81,11 @@ const Banner: React.FC<BannerProps> = ({}) => {
                             <Wrapper class="banner-contact max-h-[42px] max-w-[170px]">
                                 <Link
                                     to="contact"
-                                    className="btn-contact flex items-center bg-[#8d448b]  rounded-md "
+                                    className="btn-contact flex items-center   rounded-md "
                                     offset={-70}
                                 >
                                     <p className="py-4 px-10 text-[#ffffff]">Contact me</p>
-                                    <span className="flex items-center justify-center text-white bg-[#7a3b78] h-[42px] w-[40px]">
+                                    <div className="flex items-center justify-center text-white  h-[42px] w-[40px]">
                                         <svg
                                             className="contact-icon "
                                             x="2.5rem"
@@ -96,7 +96,7 @@ const Banner: React.FC<BannerProps> = ({}) => {
                                                 <path d="M437.5,59.3h-401C16.4,59.3,0,75.7,0,95.8v282.4c0,20.1,16.4,36.5,36.5,36.5h401c20.1,0,36.5-16.4,36.5-36.5V95.8   C474,75.7,457.6,59.3,437.5,59.3z M432.2,86.3L239.5,251.1L46.8,86.3H432.2z M447,378.2c0,5.2-4.3,9.5-9.5,9.5h-401   c-5.2,0-9.5-4.3-9.5-9.5V104.9l203.7,174.2c0.1,0.1,0.3,0.2,0.4,0.3c0.1,0.1,0.3,0.2,0.4,0.3c0.3,0.2,0.5,0.4,0.8,0.5   c0.1,0.1,0.2,0.1,0.3,0.2c0.4,0.2,0.8,0.4,1.2,0.6c0.1,0,0.2,0.1,0.3,0.1c0.3,0.1,0.6,0.3,1,0.4c0.1,0,0.3,0.1,0.4,0.1   c0.3,0.1,0.6,0.2,0.9,0.2c0.1,0,0.3,0.1,0.4,0.1c0.3,0.1,0.7,0.1,1,0.2c0.1,0,0.2,0,0.3,0c0.4,0,0.9,0.1,1.3,0.1l0,0l0,0   c0.4,0,0.9,0,1.3-0.1c0.1,0,0.2,0,0.3,0c0.3,0,0.7-0.1,1-0.2c0.1,0,0.3-0.1,0.4-0.1c0.3-0.1,0.6-0.2,0.9-0.2c0.1,0,0.3-0.1,0.4-0.1   c0.3-0.1,0.6-0.2,1-0.4c0.1,0,0.2-0.1,0.3-0.1c0.4-0.2,0.8-0.4,1.2-0.6c0.1-0.1,0.2-0.1,0.3-0.2c0.3-0.2,0.5-0.3,0.8-0.5   c0.1-0.1,0.3-0.2,0.4-0.3c0.1-0.1,0.3-0.2,0.4-0.3L447,109.2V378.2z" />
                                             </g>
                                         </svg>
-                                    </span>
+                                    </div>
                                 </Link>
                             </Wrapper>
                         </Wrapper>
@@ -108,7 +108,12 @@ const Banner: React.FC<BannerProps> = ({}) => {
                             ariaLabel="Social Links"
                             animation="fade-right"
                         >
-                            <a className="social-link" href="#" target="_blank">
+                            <a
+                                className="social-link"
+                                href="https://www.facebook.com/shamlo.redzx"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
                                 <Image
                                     src="/images/facebook.svg"
                                     alt="Social Icon Facebook"
@@ -117,7 +122,12 @@ const Banner: React.FC<BannerProps> = ({}) => {
                                     height={25}
                                 />
                             </a>
-                            <a className="social-link" href="#" target="_blank">
+                            <a
+                                className="social-link"
+                                href="https://twitter.com/Shamlo_"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
                                 <Image
                                     src="/images/twitter.svg"
                                     alt="Social Icon twitter"
@@ -126,7 +136,12 @@ const Banner: React.FC<BannerProps> = ({}) => {
                                     height={25}
                                 />
                             </a>
-                            <a className="social-link" href="#" target="_blank">
+                            <a
+                                className="social-link"
+                                href="https://github.com/shammlo"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
                                 <Image
                                     src="/images/github.svg"
                                     alt="Social Icon github"
