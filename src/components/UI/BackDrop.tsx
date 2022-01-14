1dc55763d639519c64c9a1ec24d494a127f5b3b4
sham <shamlooo@yahoo.com>
2022-01-14 20:09:28 +0300

    Fixed a backdrop closing issue

--- src/components/UI/BackDrop.tsx
+++ src/components/UI/BackDrop.tsx
@@ -8,13 +8,14 @@ interface BackDropProps {
 }
 
 const BackDrop: React.FC<BackDropProps> = ({ show, clicked }) => {
-    useEffect(() => {
-        if (show) {
-            document.body.style.overflow = 'hidden';
-        } else {
-            document.body.style.overflow = 'unset';
-        }
-    }, [show]);
+    console.log(show);
+    // useEffect(() => {
+    //     if (show) {
+    //         document.body.style.overflow = 'hidden';
+    //     } else {
+    //         document.body.style.overflow = 'unset';
+    //     }
+    // }, [show]);
     return show ? <div className="backdrop" onClick={clicked}></div> : null;
 };
 export default BackDrop;
--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -12,18 +12,35 @@ interface ModalProps {
 
 const Modal: React.FC<ModalProps> = ({ show, modalClosed, children }) => {
     return (
-        <>
+        <Wrapper class="modal">
             <BackDrop show={show} clicked={modalClosed} />
             <Wrapper
-                class="modal"
+                class="modal-overlay"
                 style={{
                     transform: show ? 'translateY(0)' : 'translateY(-200vh)',
                     opacity: show ? '1' : '0',
                 }}
             >
-                {children}
+                <Wrapper class="modal-content">
+                    <Wrapper class="modal-close" clicked={modalClosed}>
+                        <svg
+                            stroke="currentColor"
+                            fill="currentColor"
+                            strokeWidth="0"
+                            viewBox="0 0 512 512"
+                            color="#02073E"
+                            height="24px"
+                            width="24px"
+                            xmlns="http://www.w3.org/2000/svg"
+                            style={{ color: 'rgb(2, 7, 62)' }}
+                        >
+                            <path d="M405 136.798L375.202 107 256 226.202 136.798 107 107 136.798 226.202 256 107 375.202 136.798 405 256 285.798 375.202 405 405 375.202 285.798 256z"></path>
+                        </svg>
+                    </Wrapper>
+                    {children}
+                </Wrapper>
             </Wrapper>
-        </>
+        </Wrapper>
     );
 };
 export default Modal;
