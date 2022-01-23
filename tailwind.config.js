cbf888bab10675e0c94f582faed5fbc88ca5dce9
sham <shamlooo@yahoo.com>
2022-01-23 22:30:21 +0300

    Changed tailwind config to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 module.exports = {
-    mode: 'jit',
-
-    purge: [
+    content: [
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
         './public/**/*.html',
         './src/**/*.{js,jsx,ts,tsx,vue}',
     ],
-    darkMode: false, // or 'media' or 'class'
+    media: false, // or 'media' or 'class'
     theme: {
         extend: {},
         screens: {
