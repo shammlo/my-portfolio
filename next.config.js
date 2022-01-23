06ed6d86f0ea5056a0a752bea9d162f1fdf2270e
sham <shamlooo@yahoo.com>
2022-01-23 22:29:14 +0300

    Add env files

--- next.config.js
+++ next.config.js
@@ -9,4 +9,10 @@ module.exports = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
+
+    env: {
+        emailjs_server: 'service_29al46l',
+        emailjs_template: 'template_lpc48he',
+        emailjs_user: 'user_ZvmIDRZKN0Dpu9EnexPlA',
+    },
 };
