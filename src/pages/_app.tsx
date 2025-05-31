8c6dde904fad877e5428dffb4a929ec7a56b0964
shammlo <shamlooo@yahoo.com>
2025-05-31 23:40:49 +0300

    removing a comment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,7 @@
-// import '../styles/globals.css'
-import '../styles/sass/main.scss';
-import Layout from '../Layout/Layout';
-import type { AppProps } from 'next/app';
 import 'aos/dist/aos.css';
+import type { AppProps } from 'next/app';
+import Layout from '../Layout/Layout';
+import '../styles/sass/main.scss';
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <Layout>
